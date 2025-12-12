cf1828c838d4247d7f9bbc313d59145c48e8ec17
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-12-12 17:53:32 -0500

    Initial commit.

---
+++ spec/rxerces_shared.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rxerces'
+
+RSpec.shared_examples RXerces do
+  example 'version number is set to the expected value' do
+    expect(RXerces::VERSION).to eq('0.2.0')
+    expect(RXerces::VERSION).to be_frozen
+  end
+end
